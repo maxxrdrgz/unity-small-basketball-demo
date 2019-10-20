7fe2cf0cd32a9fa0d26967d9399e4780af6e4d76
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-10-19 19:24:21 -0700

    added main menu controller, updated main menu scene

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    private Animator mainAnim, ballAnim;
+    [SerializeField]
+    private GameObject mainHolder, ballHolder; 
+
+    private void Awake() {
+        mainAnim = mainHolder.GetComponent<Animator>();
+        ballAnim = ballHolder.GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +25,19 @@ public class MainMenuController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlayGame(){
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void SelectBall(){
+        mainAnim.Play("FadeOut");
+        ballAnim.Play("FadeIn");
+    }
+
+    public void BackToMenu(){
+        mainAnim.Play("FadeIn");
+        ballAnim.Play("FadeOut");
     }
 }
