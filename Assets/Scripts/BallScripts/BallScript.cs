439c146b33f74ad83b5932aafe6f3520ddeeb40e
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-10-20 13:38:53 -0700

    added ball script which plays a sound upon collision, added audio source to game manager, and added back to main menu button to gameplay scene

---
+++ Assets/Scripts/BallScripts/BallScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private int touchedGround = 0;
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Rim"){
+            if(GameManager.instance != null){
+                if(Random.Range(0, 2) > 1){
+                    GameManager.instance.PlaySound(2);
+                }else{
+                    GameManager.instance.PlaySound(5);
+                }
+            }
+        }
+        if(other.gameObject.tag == "Holder"){
+            if(GameManager.instance != null){
+                if(Random.Range(0, 2) > 1){
+                    GameManager.instance.PlaySound(3);
+                }else{
+                    GameManager.instance.PlaySound(4);
+                }
+            }
+        }
+        if(other.gameObject.tag == "Ground"){
+            touchedGround++;
+            if(touchedGround <= 3){
+                if(GameManager.instance != null){
+                    if(Random.Range(0, 2) > 1){
+                        GameManager.instance.PlaySound(3);
+                    }else{
+                        GameManager.instance.PlaySound(4);
+                    }
+                }
+            }
+        }
+        if(other.gameObject.tag == "Backboard"){
+            if(GameManager.instance != null){
+                if(Random.Range(0, 2) > 1){
+                    GameManager.instance.PlaySound(2);
+                }else{
+                    GameManager.instance.PlaySound(5);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Net"){
+            if(GameManager.instance != null){
+                GameManager.instance.PlaySound(1);
+            }
+        }
+    }
+}
--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -7,12 +7,17 @@ public class GameManager : MonoBehaviour
 {
 
     public static GameManager instance;
+    private AudioSource audio;
+    private float volume = 1.0f;
+    [SerializeField]
+    private AudioClip rim_hit1, rim_hit2, bounce1, bounce2, net_sound; 
 
     private int index = 0;
     private BallCreator ballCreator;
     private void Awake() {
         MakeSingleton();
         ballCreator = GetComponent<BallCreator>();
+        audio = GetComponent<AudioSource>();
     }
 
     void MakeSingleton(){
@@ -35,6 +40,42 @@ public class GameManager : MonoBehaviour
     void OnLevelWasLoaded(){
         if(SceneManager.GetActiveScene().name == "Gameplay"){
             CreateBall();
+        }
+    }
+
+    public void PlaySound(int id){
+        switch(id){
+            case 1:
+                audio.PlayOneShot(net_sound, volume);
+                break;
+            case 2:
+                if(Random.Range(0, 2) > 1){
+                    audio.PlayOneShot(rim_hit1, volume);
+                }else{
+                    audio.PlayOneShot(rim_hit2, volume);
+                }
+                break;
+            case 3:
+                if(Random.Range(0, 2) > 1){
+                    audio.PlayOneShot(bounce1, volume);
+                }else{
+                    audio.PlayOneShot(bounce2, volume);
+                }
+                break;
+            case 4:
+                if(Random.Range(0, 2) > 1){
+                    audio.PlayOneShot(bounce1, volume/2);
+                }else{
+                    audio.PlayOneShot(bounce2, volume/2);
+                }
+                break;
+            case 5:
+                if(Random.Range(0, 2) > 1){
+                    audio.PlayOneShot(rim_hit1, volume/2);
+                }else{
+                    audio.PlayOneShot(rim_hit2, volume/2);
+                }
+                break;
         }
     }
 }
